5730d4af55c52e4619b243df4c7deaf7c8d245ec
Justin Barner <justinrb14@gmail.com>
2021-08-29 04:44:36 -0700

    did the full code including a slider, check boxes and a copy button. generate button works. readme filled out.

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # Gizem
+
+## Description
+
+    For this project I needed to write the javascript to create a functioning password generator that would allow you to randomly produce passwords with upper and lower case letters as well as symbols and numbers. I learned how to create a working generate button that checks the parameters specified by check boxes and a range slider. Also, I was able to call a funtion to run in another function allowing the generated password to display. 
+
+## Installation
+
+* copy ssh key from [https://github.com/Jahbeezy/Gizem] and use git clone to download the file.
+
+## Usage
+
+* Open up the webpage [https://jahbeezy.github.io/Gizem/]
+and select atleast one of the parameters.
+* Slide the Character Amount to desired number
+* Use Copy button to copy the password to your clipboard
+
+<img src="./images/cap.png">
+
+ 
+
+## Credits
+
+[https://www.youtube.com/watch?v=iKo9pDKKHnc&ab_channel=WebDevSimplified]
+
+[https://www.youtube.com/watch?v=-Dog-Wd_RLw&ab_channel=Omerko]
+
+[https://developer.mozilla.org/en-US/docs/Web/JavaScript]
+
+[https://www.w3schools.com/js/default.asp]
+
+## Liscence
+
+    MIT License
+
+    Copyright (c) [year] [fullname]
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
---
+++ script.js
@@ -0,0 +1,91 @@
+// grabbed selectors and buttons from html and added them to the java via querySelector
+var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy")
+var charRange = document.querySelector("#charRange");
+var charNumber = document.querySelector("#charNumber");
+var caps = document.querySelector("#caps");
+var lows = document.querySelector("#lows")
+var numbers = document.querySelector("#numbers");
+var symbs = document.querySelector("#symbols");
+var passwordText = document.querySelector("#password");
+
+
+//created variables using character codes and function minMax
+var upperCodes = minMax(65,90);
+var lowerCodes = minMax(97,122);
+var numbCodes = minMax(48,57);
+var symbsCodes = minMax(33, 47).concat(minMax(58, 64)).concat(minMax(91, 96)).concat(minMax(123, 126));
+var none = []
+//syncs the value of the slider and range number together
+charNumber.addEventListener("input", syncCharacterAmount);
+charRange.addEventListener("input", syncCharacterAmount);
+
+//writePassword checks character amount value and if the boxes for upper, numbers, and symbols are checked.
+//Also calls the generate password function passing the new variables through
+//prints out the randomly generated password
+function writePassword(){
+  var charAmount = charNumber.value;
+  var upper = caps.checked;
+  var lowes = lows.checked;
+  var numbies = numbers.checked;
+  var symbols = symbs.checked;
+  var password = generatePassword(charAmount, upper, lowes, numbies, symbols);
+  passwordText.innerText = password;
+}
+
+//generatePassword function declares var charCode = lowerCodes making the default lowercase
+//concatonates the charCode if boxes are checked
+//a for loop with index = 0 and less than charAmount and adds 1 each time
+//Math randomly cycles through which type is selected
+//push adds the new code into the empty array
+//return the passwordFinal array as a string
+function generatePassword(charAmount, upper, lowes, numbies, symbols) {
+    
+    var charValue = none
+    if(upper) charValue = charValue.concat(upperCodes)
+    if(lowes) charValue = charValue.concat(lowerCodes)
+    if(numbies) charValue = charValue.concat(numbCodes)
+    if(symbols) charValue = charValue.concat(symbsCodes)
+    
+    
+    var passwordFinal = []
+    
+    for(var i = 0; i < charAmount; i++) {
+      var codes = charValue[Math.floor(Math.random() * charValue.length)]
+      passwordFinal.push(String.fromCharCode(codes))
+    }
+    return passwordFinal.join("");
+  }
+
+
+//The maxMin function is used to loop through the character codes declared above
+//index begins at the lowest number and cycles through by 1 until its reached the max
+//return value of the loop into the array
+function minMax(min, max){
+  var loop = []
+  for(var i = min; i <= max; i++){
+    loop.push(i)
+  }
+  return loop;
+}
+
+
+//selects the generated password and copies it
+function copyFunction(){
+  password.select()
+  document.execCommand("Copy")
+}
+
+//this function selects the range and number and makes them equal in value
+function syncCharacterAmount(event) {
+  var value = event.target.value
+  charNumber.value = value
+  charRange.value = value
+}
+// event listeners that trigger different functions when a button is clicked
+generateBtn.addEventListener("click", writePassword);
+
+copyBtn.addEventListener("click", copyFunction)
+
+
+ 
