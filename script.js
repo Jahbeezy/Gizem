7f1b28d5fe809b46ef5e26f2dc635bdeb63e3f90
Justin Barner <justinrb14@gmail.com>
2021-08-30 07:54:37 -0700

    updated comments for clarification on js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-// grabbed selectors and buttons from html and added them to the java via querySelector
+// buttons
 var generateBtn = document.querySelector("#generate");
 var copyBtn = document.querySelector("#copy")
+//range bar & number box
 var charRange = document.querySelector("#charRange");
 var charNumber = document.querySelector("#charNumber");
+//check boxes
 var caps = document.querySelector("#caps");
 var lows = document.querySelector("#lows")
 var numbers = document.querySelector("#numbers");
 var symbs = document.querySelector("#symbols");
+//password area
 var passwordText = document.querySelector("#password");
 
 
@@ -15,15 +18,16 @@ var upperCodes = minMax(65,90);
 var lowerCodes = minMax(97,122);
 var numbCodes = minMax(48,57);
 var symbsCodes = minMax(33, 47).concat(minMax(58, 64)).concat(minMax(91, 96)).concat(minMax(123, 126));
+//none for default
 var none = []
 
 //syncs the value of the slider and range number together
 charNumber.addEventListener("input", syncCharacterAmount);
 charRange.addEventListener("input", syncCharacterAmount);
 
-//writePassword checks character amount value and if the boxes for upper, numbers, and symbols are checked.
-//Also calls the generate password function passing the new variables through
-//prints out the randomly generated password
+//writePassword sets the character amount value and sets var for upper, numbers, and symbols = to checked
+//sets password = generatePassword function and passes the new variables
+//prints out the randomly generated password as text
 function writePassword(){
   var charAmount = charNumber.value;
   var upper = caps.checked;
@@ -37,13 +41,13 @@ function writePassword(){
 
 }
 
-//generatePassword function declares var charCode = lowerCodes making the default lowercase
-//concatonates the charCode if boxes are checked
-//a for loop with index = 0 and less than charAmount and adds 1 each time
-//Math randomly cycles through which type is selected
-//push adds the new code into the empty array
-//return the passwordFinal array as a string
+//generatePassword function declares var charValue = to an empty array
+//concatonates the charValue if boxes are checked for character types
 //if nothing is selected an alert is triggered
+//create empty array to pass the final password once generated
+//a for loop to create the password where index cycles from 0 adding a new random selection from charValue until it reaches the charAmount
+//push adds the new code into the empty array
+//return the passwordFinal array as a string instead of charCodes with .join
 function generatePassword(charAmount, upper, lowes, numbies, symbols) {
     
     var charValue = none
@@ -65,8 +69,9 @@ function generatePassword(charAmount, upper, lowes, numbies, symbols) {
 
 
 //The maxMin function is used to loop through the character codes declared above
-//index begins at the lowest number and cycles through by 1 until its reached the max
-//return value of the loop into the array
+//index begins at the lowest number of the array and cycles through each by 1 until its reaches the end of the array
+//This function goes into the code variables and includes every char code between the vlaues
+//return value of the loop array
 function minMax(min, max){
   var loop = []
   for(var i = min; i <= max; i++){
